680da6c50ac502778d89636d5658da7ebbf28fbf
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-01-31 14:23:49 +0100

    ok

--- js/site.js
+++ js/site.js
@@ -1,30 +1,33 @@
 gsap.registerPlugin(ScrollTrigger)
 
-const images = document.querySelectorAll('.img');
+let animTime = 0.1;
 
-// Here is where the z-index is being set
-// from greatest to least
-images.forEach(function (e, i) {
-  e.style.zIndex = images.length + 1 - i;
-});
-
-var tl = gsap.timeline({
+let tlWrapper = gsap.timeline({
   scrollTrigger: {
-    trigger: ".purple",
-    scrub: true,
-    pin: true,
+    trigger: ".js-pin-container",
+    pin: ".js-pin-container",
     start: "top top",
-    end: "+=300%",
+    end: "+=800%",
+    scrub: 1
   }
 });
 
-tl.from(".line-3", { scaleX: 0, transformOrigin: "left center", ease: "none" }, 1)
-  .to(images[0], 5, { scale: 1.2 }) // Zoom in effect
-  .to(images[0], 1.4, { autoAlpha: 0 })
-  .to(images[1], 8, { autoAlpha: 0, scale: 1.2 }, '-=1') // Zoom in effect
-  .to(images[1], 1.4, { autoAlpha: 0 })
-  .to(images[2], 5, { autoAlpha: 1, scale: 1.2 }, '-=1') // Zoom in effect
-  .to(images[2], 1.4, { autoAlpha: 0 });
+tlWrapper.from('.number_one', { autoAlpha: 0, duration: 0.5 }, animTime);
+animTime += 0.5;
+tlWrapper.from('.number_two', { autoAlpha: 0, duration: 0.5 }, animTime);
+animTime += 0.5;
+tlWrapper.from('.number_three', { autoAlpha: 0, duration: 0.5 }, animTime);
+animTime += 0.5;
+
+
+// tl.from(".line-3", { scaleX: 0, transformOrigin: "left center", ease: "none" }, 1)
+//   .to(images[0], 5, { scale: 1.2 }) // Zoom in effect
+//   .to(images[0], 5, { autoAlpha: 0 })
+//   .to(images[1], 10, { autoAlpha: 0, scale: 1.2 }) // Zoom in effect
+//   .to(images[1], 5, { autoAlpha: 0 })
+//   .to(images[2], 5, { autoAlpha: 1, scale: 1.2 }) // Zoom in effect
+//   .to(images[2], 1.4, { autoAlpha: 0 })
+//   .to(images[0], 5, { scale: 1.2 });
 
 
 gsap.utils.toArray(".revealUp").forEach(function (elem) {
